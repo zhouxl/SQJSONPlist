dafe53c19582005378bff2cde719bf02602c865b
小六 <zhouxl72@126.com>
2016-01-10 16:42:53 +0800

    Initial Commit

---
+++ SQJSONPlist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SQJSONPlist
+//
+//  Created by 小六 on 1月10日.
+//  Copyright © 2016年 小六. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
